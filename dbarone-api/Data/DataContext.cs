bdd4e88823e1c15d8a64c1bfef990599d5ed825b
davidbarone <davidbarone@live.com>
2022-03-29 21:09:57 +1100

    Data Library, Start of UI project

---
+++ dbarone-api/Data/DataContext.cs
@@ -0,0 +1,34 @@
+namespace dbarone_api.Data;
+
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+public class DataContext
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+    public DataContext(IConfiguration configuration)
+    {
+        var csBuilder = new SqlConnectionStringBuilder(configuration["ConnectionStrings:dbarone"]);
+        csBuilder.Password = configuration["DatabasePassword"]; // user-secret
+        this._connectionString = csBuilder.ConnectionString;
+    }
+    public IDbConnection CreateConnection()
+        => new SqlConnection(_connectionString);
+
+    public void Execute(string sql, object? param = null)
+    {
+        // C# 8.0 implied using declarations
+        using var db = this.CreateConnection();
+        db.Execute(sql, param);
+    }
+
+    public IEnumerable<T> Query<T>(string sql, object? param = null)
+    {
+        // C# 8.0 implied using declarations
+        using var db = this.CreateConnection();
+        var items = db.Query<T>(sql, param);
+        return items ?? new List<T>();
+    }
+}
